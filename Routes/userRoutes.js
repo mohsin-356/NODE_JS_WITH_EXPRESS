6eb984b9599847b698d1bad74d56d0b5c0c8100f
mohsin-356 <mohsinsaeed356@gmail.com>
2024-12-28 18:45:01 +0500

    add helmet package for enhanced security and implement rate limiting middleware in app.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -8,19 +8,23 @@ router.route("/getAllUsers")
             .get(
                 userController.getAllUsers
                 );
+
 router.route("/updatePassword")
             .patch(
                 authController.protect,
                 userController.updatePassword
                 );
+
 router.route("/updateMe")
             .patch(
                 authController.protect,
                 userController.updateMe
             );
+
 router.route("/deleteMe")
             .delete(
                 authController.protect,
                 userController.deleteMe
             );
+            
 module.exports = router;
--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require("express"); //returns a function that creates an Express application
-const app = express();
-const morgan=require('morgan');
-const customError=require('./Utils/customError');
-const globalErrorHandler=require('./Controllers/errorController');
-const moviesRouter=require('./Routes/moviesRoutes');
-const authRouter=require('./Routes/authRoutes');
-const userRouter=require('./Routes/userRoutes');
 
-const rateLimit = require("express-rate-limit");
-app.use(morgan('dev'));
-//CREATE A SERVER
-app.use(express.json());
-app.use("/api/v1/movies", moviesRouter);//we are actually mounting this router on the '/api/v1/movies' route
-app.use("/api/v1/auth",authRouter);//we are actually mounting this router on the '/api/v1/movies' route
-app.use("/api/v1/user",userRouter);//we are actually mounting this router on the '/api/v1/movies' route
+const app = express();//calling the function to create an Express application
+
+const rateLimit = require("express-rate-limit");//rate limiting middleware to secure the application from DOS attacks
+
+const morgan=require('morgan');//HTTP request logger middleware for node.js
+
+const customError=require('./Utils/customError');
+
+const globalErrorHandler=require('./Controllers/errorController');//error handling [MIDDLEWARE(s)]
+
+const moviesRouter=require('./Routes/moviesRoutes');//[ROUTE(s)] for [MOVIE]-operations
+const authRouter=require('./Routes/authRoutes');//[ROUTE(s)] for [AUTHENTICATION]-operations
+const userRouter=require('./Routes/userRoutes');//[ROUTE(s)] for [USER]-operations
+
+let limiter = rateLimit({
+    max: 100, // limit each IP to 100 requests per windowMs
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+app.use('/api',limiter); // apply to all requests
+
+app.use(morgan('dev'));//[MIDDLEWARE] to log the HTTP requests to the console
+app.use(express.json());//[MIDDLEWARE] to parse the incoming request body to JSON format 
+
+app.use("/api/v1/movies", moviesRouter);//we are actually mounting this router on the ['/api/v1/MOVIES]'-route
+app.use("/api/v1/auth",authRouter);//we are actually mounting this router on the ['/api/v1/AUTH']-route
+app.use("/api/v1/user",userRouter);//we are actually mounting this router on the ['/api/v1/USER']-route
 app.all('*',(req,res,next)=>{
     // return res.status(404).json({
     //     status: "error",
@@ -25,5 +39,6 @@ app.all('*',(req,res,next)=>{
     const err=new customError(`Can't find ${req.originalUrl} on this server`,404);
     next(err);
 });
+
 app.use(globalErrorHandler);
 module.exports = app;
