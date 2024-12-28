d33ff0b3ed0c75df0bcd08247f3f3e88fe0a4adf
mohsin-356 <mohsinsaeed356@gmail.com>
2024-12-28 19:22:05 +0500

    add hpp middleware to prevent HTTP Parameter Pollution attacks

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app = express();//calling the function to create an Express application
 
 const rateLimit = require("express-rate-limit");//rate limiting middleware to secure the application from DOS attacks
 
-const helmet = require("helmet");//security middleware for Express.js to help you secure your applications by setting various HTTP headers
+// const helmet = require("helmet");//security middleware for Express.js to help you secure your applications by setting various HTTP headers
 
 const xss = require("xss-clean");//middleware to prevent XSS attacks
 
 const sanitize = require("express-mongo-sanitize");//middleware to prevent NoSQL injection attacks
+
+const httpParametersPollution = require("hpp");//middleware to prevent HTTP Parameter Pollution attacks
 
 const morgan=require('morgan');//HTTP request logger middleware for node.js
 
@@ -19,6 +21,7 @@ const globalErrorHandler=require('./Controllers/errorController');//error handli
 const moviesRouter=require('./Routes/moviesRoutes');//[ROUTE(s)] for [MOVIE]-operations
 const authRouter=require('./Routes/authRoutes');//[ROUTE(s)] for [AUTHENTICATION]-operations
 const userRouter=require('./Routes/userRoutes');//[ROUTE(s)] for [USER]-operations
+const { whitelist } = require("validator");
 
 let limiter = rateLimit({
     max: 100, // limit each IP to 100 requests per windowMs
@@ -33,6 +36,11 @@ app.use(express.json({limit: '10kb'}));//[GLOBAL-MIDDLEWARE] to parse the incomi
 
 app.use(sanitize());//[GLOBAL-MIDDLEWARE] to prevent NoSQL injection attacks
 app.use(xss());//[GLOBAL-MIDDLEWARE] to prevent XSS attacks
+app.use(httpParametersPollution({
+        whitelist:['duration',
+                'ratings',
+            'releaseYear',]
+}));//[GLOBAL-MIDDLEWARE] to secure the application by setting various HTTP headers
 
 app.use("/api/v1/movies", moviesRouter);//we are actually mounting this router on the ['/api/v1/MOVIES]'-route
 app.use("/api/v1/auth",authRouter);//we are actually mounting this router on the ['/api/v1/AUTH']-route
